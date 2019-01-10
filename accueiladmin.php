7c60733582991378eae2678037db252e43f07b42
Elementaruss <alex.ben2227@gmail.com>
2019-01-10 15:04:15 +0100

    Avancement2

--- accueiladmin.php
+++ accueiladmin.php
@@ -5,7 +5,9 @@
   <head>
     <meta content="text/html; charset=ISO-8859-1" http-equiv="content-type">
     <title>Connexion</title>
-    <link rel=stylesheet type="text/css" href="style.css"/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
   </head>
   <body>
       <?php
--- accueilsalarie.php
+++ accueilsalarie.php
@@ -2,12 +2,14 @@
   session_start()
 ?>
 <html>
+  <head>
+      <meta content="text/html; charset=ISO-8859-1" http-equiv="content-type">
+      <title>Connexion</title>
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  </head>
   <body>
-    <head>
-        <meta content="text/html; charset=ISO-8859-1" http-equiv="content-type">
-        <title>Connexion</title>
-        <link rel=stylesheet type="text/css" href="style.css"/>
-    </head>
       <?php
         //Vérifie si l'utilisateur est connecté avant de montrer la page
         if ($_SESSION['verif'] == "salarie")
@@ -32,11 +34,11 @@
         }
       ?>
   <?php
-    echo"Connecté en tant que : ". $_SESSION['leprenom'] ." ". $_SESSION['lenom'] ."";
+    echo"Connecte en tant que : ". $_SESSION['leprenom'] ." ". $_SESSION['lenom'] ."";
     echo"<BR><BR>";
   ?>
   <button>Proposer trajet</button>
-  <button>Voir Trajet</button>
+  <a href="voirtrajet.php"><button>Voir Trajet</button></a>
   <button>Voir Inscription</button>
   <BR><BR>
   <a href="deconnection.php"><button>Deconnexion</button></a>
